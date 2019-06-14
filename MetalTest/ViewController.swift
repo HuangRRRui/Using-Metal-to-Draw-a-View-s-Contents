9167d7d1326413c0ceb2fd5572d1b1f91ead19b4
HuangRui <13618641207@163.com>
2019-06-14 16:26:34 +0800

    Initial Commit

---
+++ MetalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetalTest
+//
+//  Created by isec on 2019/6/14.
+//  Copyright © 2019 huangrui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
